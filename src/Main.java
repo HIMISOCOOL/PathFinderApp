545aa6e38881ea384f041d7317d8a52878bdf14c
Matthew <lololirpro@gmail.com>
2014-05-12 22:50:39 +1200

    uh fixed somthing

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
-import rpg.gui.Interface;
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 /**
- * @author Real Standard Studios - Matthew Meehan
  *
- * The package for general classes like the main class of the project
- * this main is for the PC gui
+ * @author Matthew
  */
-public class Main extends Application{
+public class Main {
     
-    @Override
-    public void start(Stage stage) throws Exception {
-        Interface gui = new Interface();
-        gui.initialize(stage, "RPG App");
-    }
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
