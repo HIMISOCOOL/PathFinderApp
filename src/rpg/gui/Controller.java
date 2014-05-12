10950751147c9e9141eb6572fe4cb6be04319f79
Matthew <lololirpro@gmail.com>
2014-05-13 00:14:25 +1200

    uh fixed somthing

--- src/rpg/gui/Controller.java
+++ src/rpg/gui/Controller.java
@@ -14,49 +14,7 @@ import javafx.fxml.FXML;
  */
 public class Controller extends JefXif.Controller{
     
-    @FXML
-    private void handleMenuAction(ActionEvent event) {
-        //toggles the menu being open or closed
-    }
-    @FXML
-    private void handleCharacterAction(ActionEvent event) {
-        getWindows().get(0).windowInView();
-    }
-    @FXML
-    private void handleCombatStatsAction(ActionEvent event) {
-        getWindows().get(1).windowInView();
-    }
-    @FXML
-    private void handleInventoryAction(ActionEvent event) {
-        getWindows().get(2).windowInView();
-    }
-    @FXML
-    private void handleSkillsAction(ActionEvent event) {
-        getWindows().get(3).windowInView();
-    }
-    @FXML
-    private void handleSpellsAction(ActionEvent event) {
-        getWindows().get(4).windowInView();
-    }
-    @FXML
-    private void handleOtherCharacterAction(ActionEvent event) {
-        getWindows().get(5).windowInView();
-    }
     
-    @FXML
-    private void handleCombatAction(ActionEvent event) {
-        getWindows().get(6).windowInView();
-    }
-    
-    @FXML
-    private void handleNewCharacterAction(ActionEvent event) {
-        getWindows().get(7).windowInView();
-    }
-    
-    @FXML
-    private void handleFeatsAction(ActionEvent event) {
-        getWindows().get(8).windowInView();
-    }
     
     /**
      * Initializes the controller class.
